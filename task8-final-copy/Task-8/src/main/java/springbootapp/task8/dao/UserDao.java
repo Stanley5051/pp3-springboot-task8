ec21333d14f1e3ef2212ee0d6adca7a54f54c20c
Stanley5051 <timofeev-egor90@yandex.ru>
2024-09-30 13:54:34 +0300

    Add files via upload

---
+++ task8-final-copy/Task-8/src/main/java/springbootapp/task8/dao/UserDao.java
@@ -0,0 +1,19 @@
+package springbootapp.task8.dao;
+
+
+import springbootapp.task8.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void createUser(User user);
+
+    User getUser(Long id);
+
+    List<User> getAllUsers();
+
+    void updateUser(User user, Long id);
+
+    void deleteUser(Long id);
+
+}
